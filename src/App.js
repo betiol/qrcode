564d3bef911f0639febdbebc6e96ecadad58d45c
Nikollas Betiol <nikollasdev@gmail.com>
2023-09-26 11:55:35 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { QrReader } from "react-qr-reader";
+import invertColor from "invert-color";
 
-function App() {
+const QRCodeScanner = () => {
+  const [result, setResult] = useState(null);
+
+  const handleScan = (data) => {
+    if (data) {
+      alert(data);
+    }
+  };
+
+  const handleError = (err) => {
+    console.error(err);
+  };
+
+  const qrReaderStyle = {
+    width: "100%"
+  };
+
+  const scannerConfig = {
+    facingMode: "environment", // Use a câmera traseira, se disponível
+    delay: 300,
+    legacyMode: false, // Use o modo moderno para melhor desempenho
+    // Configurações de contraste e brilho para leitura de QR codes com fundo preto
+    constraints: {
+      video: {
+        facingMode: "environment",
+        brightness: { ideal: 1, min: 0.5, max: 2 },
+        contrast: { ideal: 1, min: 0.5, max: 2 }
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <QrReader
+        {...scannerConfig}
+        onError={handleError}
+        onResult={handleScan}
+        style={qrReaderStyle}
+      />
+      {result && (
+        <div style={{ marginTop: "20px" }}>
+          <p>Conteúdo do QR code:</p>
+          <div
+            style={{
+              backgroundColor: "white",
+              padding: "10px",
+              color: "black",
+              fontWeight: "bold"
+            }}
+          >
+            {result}
+          </div>
+          <div
+            style={{
+              marginTop: "20px",
+              backgroundColor: invertColor("white", true),
+              padding: "10px",
+              color: invertColor("black", true),
+              fontWeight: "bold"
+            }}
+          >
+            {result}
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default QRCodeScanner;
